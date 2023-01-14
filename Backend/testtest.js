ac3a182d138ef7b78835a29b9a44230e763dbf78
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2023-01-15 03:56:59 +0900

    timezone-issues-resolved

--- Backend/testtest.js
+++ Backend/testtest.js
@@ -1,3 +1,9 @@
-console.log("1");
-console.log(typeof parseInt("1"));
-console.log(typeof parseInt(1));
+import moment from 'moment-timezone';
+function DayOrNight(){
+    const krTimeHr = parseInt(moment().tz("Asia/Seoul").format('H'));
+    if(krTimeHr < 9 || krTimeHr >= 22) return "Night";
+    else return "Day";
+}
+console.log(parseInt(moment().tz("Asia/Seoul").format('HHmmss')));
+
+console.log(DayOrNight());
