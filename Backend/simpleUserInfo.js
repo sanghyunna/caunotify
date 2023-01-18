830e472d716fb35b159c3051460c6e5c2328a6e6
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2023-01-18 17:09:59 +0900

    Update simpleUserInfo.js

--- Backend/simpleUserInfo.js
+++ Backend/simpleUserInfo.js
@@ -5,13 +5,16 @@ export function simpleUserInfo(user){
     // 전체 공지
     if(user.CAUnotice == "true") res = res.concat(', ',KRname("CAUnotice"));
     if(user.dorm == "true") res = res.concat(', ',KRname("dorm"));
+    if(user.davinci == "true") res = res.concat(', ',KRname("davinci"));
+    
     // 경경대
     if(user.business == "true") res = res.concat(', ',KRname("business"));
     if(user.logistics == "true") res = res.concat(', ',KRname("logistics"));
     if(user.econ == "true") res = res.concat(', ',KRname("econ"));
     if(user.industSec == "true") res = res.concat(', ',KRname("industSec"));
     if(user.appliedStat == "true") res = res.concat(', ',KRname("appliedStat"));
-    if(user.adpr == "true") res = res.concat(', ',KRname("ADPR"));
+    if(user.adpr == "true") res = res.concat(', ',KRname("adpr"));
+
     // 인문대
     if(user.korean == "true") res = res.concat(', ',KRname("korean"));
     if(user.english == "true") res = res.concat(', ',KRname("english"));
@@ -22,6 +25,7 @@ export function simpleUserInfo(user){
     if(user.german == "true") res = res.concat(', ',KRname("german"));
     if(user.philosophy == "true") res = res.concat(', ',KRname("philosophy"));
     if(user.history == "true") res = res.concat(', ',KRname("history"));
+
     // 사과대
     if(user.publicService == "true") res = res.concat(', ',KRname("publicService"));
     if(user.psychology == "true") res = res.concat(', ',KRname("psychology"));
@@ -31,16 +35,19 @@ export function simpleUserInfo(user){
     if(user.mediaComm == "true") res = res.concat(', ',KRname("mediaComm"));
     if(user.sociology == "true") res = res.concat(', ',KRname("sociology"));
     if(user.socialWelfare == "true") res = res.concat(', ',KRname("socialWelfare"));
+
     // 사범대
     if(user.education == "true") res = res.concat(', ',KRname("education"));
     if(user.earlyChildhoodEd == "true") res = res.concat(', ',KRname("earlyChildhoodEd"));
     if(user.englishEd == "true") res = res.concat(', ',KRname("englishEd"));
     if(user.physicalEd == "true") res = res.concat(', ',KRname("physicalEd"));
+
     // 자연대
     if(user.physics == "true") res = res.concat(', ',KRname("physics"));
     if(user.chem == "true") res = res.concat(', ',KRname("chem"));
     if(user.lifeScience == "true") res = res.concat(', ',KRname("lifeScience"));
     if(user.math == "true") res = res.concat(', ',KRname("math"));
+
     // 공과대
     if(user.chemEngineering == "true") res = res.concat(', ',KRname("chemEngineering"));
     if(user.civilEnvPlanEng == "true") res = res.concat(', ',KRname("civilEnvPlanEng"));
@@ -48,16 +55,22 @@ export function simpleUserInfo(user){
     if(user.mechEngineering == "true") res = res.concat(', ',KRname("mechEngineering"));
     if(user.enerEngineering == "true") res = res.concat(', ',KRname("enerEngineering"));
     if(user.architecture == "true") res = res.concat(', ',KRname("architecture"));
+
     // 창의ICT
     if(user.integEngineering == "true") res = res.concat(', ',KRname("integEngineering"));
     if(user.elecEngineering == "true") res = res.concat(', ',KRname("elecEngineering"));
+    if(user.polaris == "true") res = res.concat(', ',KRname("polaris"));
+
     // 소프트
     if(user.software == "true") res = res.concat(', ',KRname("software"));
     if(user.ai == "true") res = res.concat(', ',KRname("ai"));
+
     // 의
     if(user.med == "true") res = res.concat(', ',KRname("med"));
+
     // 약
     if(user.pharm == "true") res = res.concat(', ',KRname("pharm"));
+
     // 간
     if(user.nursing == "true") res = res.concat(', ',KRname("nursing"));
 
